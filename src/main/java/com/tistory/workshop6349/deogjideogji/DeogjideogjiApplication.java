3862483608f6eb94d5c80b455188dfbd5afa81f3
iqpizza6349 <iqpizza6349@gmail.com>
2022-03-24 18:17:57 +0900

    🌱 최초 커밋

---
+++ src/main/java/com/tistory/workshop6349/deogjideogji/DeogjideogjiApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.workshop6349.deogjideogji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeogjideogjiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeogjideogjiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tistory/workshop6349/deogjideogji/DeogjideogjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.workshop6349.deogjideogji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeogjideogjiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
