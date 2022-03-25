fabe35c87e9a90e0ce86d40fb103378650184785
iqpizza6349 <iqpizza6349@gmail.com>
2022-03-25 10:49:08 +0900

    ⚗::#1 OAuth Builder 어노테이션 추가

--- src/main/java/com/tistory/workshop6349/deogjideogji/domain/member/entity/OAuth.java
+++ src/main/java/com/tistory/workshop6349/deogjideogji/domain/member/entity/OAuth.java
@@ -1,6 +1,9 @@
 package com.tistory.workshop6349.deogjideogji.domain.member.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -8,6 +11,8 @@ import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Getter
+@AllArgsConstructor @NoArgsConstructor
+@Builder
 @Embeddable
 public class OAuth implements Serializable {
 
